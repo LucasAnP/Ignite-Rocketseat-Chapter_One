cadb75f751ff62830dedfa60f0b82e05f4fb7950
niubajr19 <lucas.antonio199@outlook.com>
2022-07-08 18:28:24 -0300

    Aula básica

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Welcome, Lucas</Text>
+      <TextInput
+        style={styles.inputBody}
+        placeholder={"New Skill"}
+        placeholderTextColor={"#fff"}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
     </SafeAreaView>
   );
 }
@@ -12,10 +29,37 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "black",
+
+    paddingVertical: 70,
+    paddingHorizontal: 30,
   },
   title: {
     color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  inputBody: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+
+  button: {
+    backgroundColor: "#A370F7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
